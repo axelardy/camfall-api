1447548a202ae7789fac20272c42f85601114b57
axelardy <75204732+axelardy@users.noreply.github.com>
2023-12-25 14:40:57 +0800

    more efficient decode method

--- client_socket.py
+++ client_socket.py
@@ -5,29 +5,49 @@
 import socket,cv2, pickle,struct
 import pyshine as ps # pip install pyshine
 import imutils # pip install imutils
-camera = False
+import time
+
+camera = True
 if camera == True:
 	vid = cv2.VideoCapture(0)
 else:
 	vid = cv2.VideoCapture('test.mp4')
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_ip = '192.168.2.151' # Here according to your server ip write the address
+host_ip = '169.254.83.107' # Here according to your server ip write the address
 
 port = 9999
 client_socket.connect((host_ip,port))
 
 if client_socket: 
+	start_time = time.time()
+	total_bytes_sent = 0
+	last_print_time = start_time
+
 	while (vid.isOpened()):
 		try:
 			img, frame = vid.read()
-			frame = imutils.resize(frame,width=380)
-			a = pickle.dumps(frame)
-			message = struct.pack("Q",len(a))+a
+			frame = imutils.resize(frame,width=720)
+
+			# more efficient solution
+			result, buffer = cv2.imencode('.jpg', frame)
+			buffer = buffer.tobytes()
+			message = struct.pack("Q",len(buffer))+buffer
+		
+
 			client_socket.sendall(message)
+			total_bytes_sent += len(message)
+
 			cv2.imshow(f"TO: {host_ip}",frame)
 			key = cv2.waitKey(1) & 0xFF
 			if key == ord("q"):
 				client_socket.close()
+
+			current_time = time.time()
+			if current_time - last_print_time >= 1:
+				total_time_elapsed = current_time - start_time
+				average_bytes_per_second = total_bytes_sent / total_time_elapsed
+				print(f"\rAverage bytes sent per second: {average_bytes_per_second*8/1000000} mbps", end='')
+				last_print_time = current_time
 		except:
 			print('VIDEO FINISHED!')
 			break
--- server_socket.py
+++ server_socket.py
@@ -8,6 +8,7 @@ import threading
 import pyshine as ps # pip install pyshine
 import cv2
 from ultralytics import YOLO
+import numpy as np
 
 model = YOLO("../models/best.pt")
 
@@ -40,7 +41,10 @@ def show_client(addr,client_socket):
 					data += client_socket.recv(4*1024)
 				frame_data = data[:msg_size]
 				data  = data[msg_size:]
-				frame = pickle.loads(frame_data)
+
+				frame = cv2.imdecode(np.frombuffer(frame_data, dtype=np.uint8), -1) # more efficient
+				# frame = pickle.loads(frame_data)
+
 				text  =  f"CLIENT: {addr}"
 				result = model(frame, conf=0.85, verbose=False)
 				fall_box = result[0].boxes.cpu().numpy().xyxy
